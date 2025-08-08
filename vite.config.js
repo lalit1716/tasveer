beb92abdef9a9ca0a7924f3163713a3ca49cd151
lalit <lalit@example.com>
2025-08-08 16:10:08 +0530

    Change: PWA changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "tasveer",
+    short_name: "tasveer-app",
+    description: "I am a simple vite app",
+    icons: [
+      {
+        src: "/assets/icons/tasveer.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+     
+    ],
+    theme_color: "#181818",
+    background_color: "#e0cc3b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugin)],
+});
